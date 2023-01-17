cd3575efb6ad65e4e18218c7d62613168952d4ad
Hien Huynh <hien.huynh.px@renesas.com>
2023-01-17 08:00:32 +0700

    platform: generic: renesas: rzfive: Configure the Local memory regions as part of root domain
    Renesas RZ/Five RISC-V SoC has Instruction local memory and Data local
    memory (ILM & DLM) mapped between region 0x30000 - 0x4FFFF. When a
    virtual address falls within this range, the MMU doesn't trigger a page
    fault; it assumes the virtual address is a physical address which can
    cause undesired behaviours for statically linked applications/libraries.
    
    To avoid this, add the ILM/DLM memory regions to the root domain region
    of the PMPU with permissions set to 0x0 for S/U modes so that any access
    to these regions gets blocked and for M-mode we grant full access (R/W/X).
    
    Refer link:
    https://patchwork.ozlabs.org/project/opensbi/patch/20230113164719.8058-1-prabhakar.mahadev-lad.rj@bp.renesas.com/
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- platform/renesas/rzf-dev/platform.c
+++ platform/renesas/rzf-dev/platform.c
@@ -14,6 +14,7 @@
 #include <sbi/riscv_encoding.h>
 #include <sbi/sbi_console.h>
 #include <sbi/sbi_const.h>
+#include <sbi/sbi_domain.h>
 #include <sbi/sbi_ipi.h>
 #include <sbi/sbi_platform.h>
 #include <sbi/sbi_trap.h>
@@ -126,15 +127,27 @@ static int rzf_irqchip_init(bool cold_boot)
 /* Platform early initialization. */
 static int rzf_early_init(bool cold_boot)
 {
-	int ret=0;
-
 	sbi_clear_mmiscctl_msa();
 
 	if (cold_boot) {
-		ret = plmt_cold_timer_init(RZF_PLMT_ADDR,
-					   RZF_HART_COUNT);
+		plmt_cold_timer_init(RZF_PLMT_ADDR,
+				     RZF_HART_COUNT);
 	}
-	return ret;
+
+	/*
+	 * Renesas RZ/Five RISC-V SoC has Instruction local memory and
+	 * Data local memory (ILM & DLM) mapped between region 0x30000
+	 * to 0x4FFFF. When a virtual address falls within this range,
+	 * the MMU doesn't trigger a page fault; it assumes the virtual
+	 * address is a physical address which can cause undesired
+	 * behaviours for statically linked applications/libraries. To
+	 * avoid this, add the ILM/DLM memory regions to the root domain
+	 * region of the PMPU with permissions set to 0x0 for S/U modes
+	 * so that any access to these regions gets blocked and for M-mode
+	 * we grant full access.
+	*/
+	return sbi_domain_root_add_memrange(0x30000, 0x20000, 0x1000,
+					    SBI_DOMAIN_MEMREGION_M_RWX);
 }
 
 /* Initialize platform timer for current HART. */
